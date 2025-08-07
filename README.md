e5970e4aecba5e49fb4aff2861f1ec56d65e9c65
Abhay Rajak <102440531+themountainboy19@users.noreply.github.com>
2025-08-07 16:23:04 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Terraform Docker Container Task
+
+This project provisions an Nginx Docker container using Terraform.
+
+## Files
+- `main.tf`: Terraform configuration file
+- `execution_logs.txt`: Sample logs from running Terraform commands
+
+## Commands Used
+```bash
+terraform init
+terraform plan
+terraform apply -auto-approve
+docker ps
+terraform destroy -auto-approve
+```
---
+++ main.tf
@@ -0,0 +1,26 @@
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "my-nginx-container"
+  image = docker_image.nginx.name
+
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
